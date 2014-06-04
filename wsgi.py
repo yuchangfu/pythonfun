5bf895c58faeefaefdde3be9a6e3f5354ef60728
yuchangfu <yuchangfu@foxmail.com>
2014-06-04 21:06:10 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,6 @@ site.addsitedir(os.path.dirname(__file__),'packages')
 
 from flask import Flask
 application = Flask(__name__)
-application.DEBUG=True
-application.run()
 @app.route("/")
 def hello():
     return "Hello yuc!"
